0c49b6ef296cee21b404dae7063df3f757876e83
Zeno <mdubos@gaming.tech>
2024-04-05 10:56:46 +0200

    ok

--- erm.py
+++ erm.py
@@ -1,96 +1,135 @@
-import pygame as py
+# CODE A ETUDIER ET RETRAVAILLER POUR LE SYSTEME DE PROJECTILE D'ARME
 
-WIDTH = 1600
-HEIGHT = 900
-BACKGROUND = (0, 128, 0)
-py.display.set_caption("GEODYSSEY")
+import pygame
+pygame.init()
 
-class Sprite(py.sprite.Sprite):
-    def __init__(self, image, startx, starty):
-        super().__init__()
-        self.image = py.image.load(image)
-        self.rect = self.image.get_rect()
-        self.rect.center = [startx, starty]
+win = pygame.display.set_mode((500,480))
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+pygame.display.set_caption("First Game")
 
-class Ground(Sprite):
-    def __init__(self, startx, starty):
-        super().__init__("ground.jpg", startx, starty)
+walkRight = pygame.image.load('IMAGES\circle.png')
+walkLeft = pygame.image.load('IMAGES\circle.png')
+bg = pygame.image.load('IMAGES\BG.png')
+char = pygame.image.load('IMAGES\circle.png')
 
-class Player(Sprite):
-    def __init__(self, startx, starty):
-        super().__init__("circle.png", startx, starty)
-        self.image = py.transform.scale(self.image, (1500, 1500))
-        self.speed = 5
-        self.jump = 10
-        self.verticalSpeed = 0
-        self.gravity = 1
+clock = pygame.time.Clock()
 
-    def update(self, ground):
-        horizontalSpeed = 0
-        onGround = self.checkCollision(0, 1, ground)
 
-        key = py.key.get_pressed()
-        if key[py.K_q]:
-            horizontalSpeed = -self.speed
-        elif key[py.K_d]:
-            horizontalSpeed = self.speed
+class player(object):
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+        self.standing = True
 
-        if key[py.K_z] and onGround:
-            self.verticalSpeed = -self.jump
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
 
-        if self.verticalSpeed < 10 and not onGround:
-            self.verticalSpeed += self.gravity
-        
-        if self.verticalSpeed > 0 and onGround:
-            self.verticalSpeed = 0
-        
-        self.move(horizontalSpeed, self.verticalSpeed, ground)
+        if not(self.standing):
+            if self.left:
+                win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+            elif self.right:
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount +=1
+        else:
+            if self.right:
+                win.blit(walkRight, (self.x, self.y))
+            else:
+                win.blit(walkLeft, (self.x, self.y))
+                
 
-    def move(self, x, y, boxes):
-        directionX = x
-        directionY = y
 
-        while self.checkCollision(0, directionY, boxes):
-            directionY -= 1
+class projectile(object):
+    def __init__(self,x,y,radius,color,facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
 
-        while self.checkCollision(directionX, directionY, boxes):
-            directionX -= directionX / abs(directionX)
-        
-        self.rect.move_ip([directionX, directionY])
+    def draw(self,win):
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
 
-    def checkCollision(self, x, y, boxes):
-        self.rect.move_ip([x, y])
-        collide = py.sprite.spritecollideany(self, boxes)
-        self.rect.move_ip([-x, -y])
-        return collide
+
+
+def redrawGameWindow():
+    win.blit(bg, (0,0))
+    man.draw(win)
+    for bullet in bullets:
+        bullet.draw(win)
     
-def main():
-    run = True
-    py.init()
-    screen = py.display.set_mode((WIDTH, HEIGHT))
-    clock = py.time.Clock()
-    player = Player(100, 200)
-    ground = py.sprite.Group()
+    pygame.display.update()
 
-    for bx in range(0, 800, 32):
-        ground.add(Ground(bx, 1100))
 
-    while run:
-        py.event.pump()
-        player.update(ground)
+#mainloop
+man = player(200, 410, 64,64)
+bullets = []
+run = True
+while run:
+    clock.tick(27)
 
-        for event in py.event.get():
-            if event.type == py.QUIT:
-                run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    for bullet in bullets:
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
 
-        screen.fill(BACKGROUND)
-        player.draw(screen)
-        ground.draw(screen)
-        py.display.update()
-        clock.tick(60)
+    keys = pygame.key.get_pressed()
 
-if __name__ == "__main__":
-    main()
+    if keys[pygame.K_SPACE]:
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+            
+        if len(bullets) < 5:
+            bullets.append(projectile(round(man.x + man.width //2), round(man.y + man.height//2), 6, (0,0,0), facing))
+
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+        man.standing = False
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
+        man.standing = False
+    else:
+        man.standing = True
+        man.walkCount = 0
+        
+    if not(man.isJump):
+        if keys[pygame.K_UP]:
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
+    else:
+        if man.jumpCount >= -10:
+            neg = 1
+            if man.jumpCount < 0:
+                neg = -1
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg
+            man.jumpCount -= 1
+        else:
+            man.isJump = False
+            man.jumpCount = 10
+            
+    redrawGameWindow()
+
+pygame.quit()

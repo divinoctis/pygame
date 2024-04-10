f18cd3e73b4a74f0eaa2eed6620a175e55b35dee
Zeno <mdubos@gaming.tech>
2024-04-10 21:53:23 +0200

    connexion DE MRD

--- Test map level editor/map.py
+++ Test map level editor/map.py
@@ -36,4 +36,4 @@ class Map:
 
     def draw(self):
         [pygame.draw.rect(self.game.screen, 'darkgray', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
-         for pos in self.world_map]
+        for pos in self.world_map]

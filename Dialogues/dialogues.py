af7784ca28777046fc99218c983eb6a83c5d1b23
Zeno <mdubos@gaming.tech>
2024-04-11 09:57:01 +0200

    erm

--- Dialogues/dialogues.py
+++ Dialogues/dialogues.py
@@ -3,15 +3,15 @@ import pygame
 # BASE DIALOGUE, JUSTE POUR TESTER.
 
 pygame.init()
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((1600, 900))
 clock = pygame.time.Clock()
 
 box = pygame.Rect(300, 200, 100, 100)
 player = pygame.Rect(50, 50, 30, 30)
 
-font = pygame.font.SysFont('Times New Roman', 25)
-texts = ['Hi', 'Hello']
-text_renders = [font.render(text, True, (0, 0, 255)) for text in texts]
+font = pygame.font.Font("Dialogues/04b_25__.ttf", 15)
+texts = ['LORA CROUS: Avec mon tombe radar, impossible de louper le tresor !', 'DAFFY DOC: La route ? La ou on va, on n’a pas besoin de route !']
+text_renders = [font.render(text, True, (255, 255, 255)) for text in texts]
 index = -1
 a_released = True
 
@@ -42,7 +42,7 @@ while True:
         index = -1
 
     if index != -1:
-        screen.blit(text_renders[index], (0, 0))
+        screen.blit(text_renders[index], (50, 50))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
